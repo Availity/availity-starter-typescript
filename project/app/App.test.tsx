25021c6cc505ec4584e462e13c46e4a1ac9e6c6f
Greg Martin <greg.martin@availity.com>
2023-08-03 13:56:52 -0400

    test: remove unused mock

--- project/app/App.test.tsx
+++ project/app/App.test.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter as Router } from 'react-router-dom';
-import axios from 'axios';
-
-import spaces from '../data/spaces.json';
 import App from './App';
-
-jest.mock('axios');
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const axiosMock =  axios as jest.Mocked<typeof axios>;
 
 describe('App', () => {
   test('renders', async () => {
-    axiosMock.mockResolvedValue({
-      config: {},
-      headers: {},
-      data: spaces,
-      status: 202,
-      statusText: 'Ok',
-    });
-
     render(
       <Router initialEntries={['/']}>
         <App />
